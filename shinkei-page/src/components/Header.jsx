d2cc5f529fa37c59c89e376cd608c9e20e75f846
Shinkei <shinkeygentatsu@yahoo.com.mx>
2018-06-21 23:02:30 -0500

    Delete the template and instead add the src to the react project
    remove the template, add the src to the react project  and change the
    header as a react component

--- shinkei-page/src/components/Header.jsx
+++ shinkei-page/src/components/Header.jsx
@@ -5,26 +5,45 @@ const Header = () => (
   <header className="header_area">
     <div className="container">
       <nav className="navbar navbar-default">
-
         <div className="navbar-header">
-          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
+            aria-expanded="false"
+          >
             <span className="sr-only">Toggle navigation</span>
             <span className="icon-bar" />
             <span className="icon-bar" />
             <span className="icon-bar" />
           </button>
-          <a className="navbar-brand" href="index.html"><img src="img/logo.png" alt="" /></a>
+          <h1 className="my-name"><strong>JORGE RAMIREZ</strong></h1>
         </div>
 
         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul className="nav navbar-nav navbar-right">
-            <li className="active"><a href="#about">ABOUT ME </a></li>
-            <li><a href="#skill">Skill</a></li>
-            <li><a href="#education">Education</a></li>
-            <li><a href="#service">SERVICES</a></li>
-            <li><a href="#portfolio">PORTFOLIO</a></li>
-            <li><a href="#news">News</a></li>
-            <li><a href="#contact">CONTACT</a></li>
+            <li className="active">
+              <a href="#about">ABOUT ME </a>
+            </li>
+            <li>
+              <a href="#skill">Skill</a>
+            </li>
+            <li>
+              <a href="#education">Education</a>
+            </li>
+            <li>
+              <a href="#service">SERVICES</a>
+            </li>
+            <li>
+              <a href="#portfolio">PORTFOLIO</a>
+            </li>
+            <li>
+              <a href="#news">News</a>
+            </li>
+            <li>
+              <a href="#contact">CONTACT</a>
+            </li>
           </ul>
         </div>
       </nav>
