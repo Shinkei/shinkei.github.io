428c8a1165edcf328747a541d7ca1df5fe36fd83
shinkei <ing.jorgeivanramirezh@gmail.com>
2018-06-28 16:47:59 -0500

    add information for the projects section

--- shinkei-page/src/components/Projects.jsx
+++ shinkei-page/src/components/Projects.jsx
@@ -11,78 +11,78 @@ const Projects = () => (
         <div className="col-md-4 photo marketing">
           <div className="portfolio_item">
             <div className="portfolio_img">
-              <img src="img/portfolio/portfolio-1.png" alt="" />
+              <img src="img/portfolio/tomate.png" alt="" width="315" height="280" />
             </div>
             <div className="portfolio_title">
-              <a href="#about">
-                <h4>Dreams visualization</h4>
+              <a href="https://gitlab.com/shinkei/pomodoro">
+                <h4>Pomodoro</h4>
               </a>
-              <h5>Development</h5>
+              <h5>Script to use the pomodoro technique through the terminal</h5>
             </div>
           </div>
         </div>
         <div className="col-md-4 design">
           <div className="portfolio_item">
             <div className="portfolio_img">
-              <img src="img/portfolio/portfolio-2.png" alt="" />
+              <img src="img/portfolio/firstaid.png" alt="" width="315" height="280" />
             </div>
             <div className="portfolio_title">
-              <a href="#about">
-                <h4>Dreams visualization</h4>
+              <a href="https://gitlab.com/manito/PhaserInit">
+                <h4>PhaserInit</h4>
               </a>
-              <h5>Development</h5>
+              <h5>Little game made in phaser and package with webpack</h5>
             </div>
           </div>
         </div>
         <div className="col-md-4 photo marketing">
           <div className="portfolio_item">
             <div className="portfolio_img">
-              <img src="img/portfolio/portfolio-3.png" alt="" />
+              <img src="img/portfolio/natgeo.jpg" alt="" width="315" height="280" />
             </div>
             <div className="portfolio_title">
-              <a href="#about">
-                <h4>Dreams visualization</h4>
+              <a href="https://github.com/Ondama/hello_world">
+                <h4>Natgeo Wallpaper</h4>
               </a>
-              <h5>Development</h5>
+              <h5>Little rust application that set as wallpaper the photo of the day of National Geographic site</h5>
             </div>
           </div>
         </div>
         <div className="col-md-4 design marketing">
           <div className="portfolio_item">
             <div className="portfolio_img">
-              <img src="img/portfolio/portfolio-4.png" alt="" />
+              <img src="img/portfolio/fire.jpg" alt="" width="315" height="280" />
             </div>
             <div className="portfolio_title">
-              <a href="#about">
-                <h4>Dreams visualization</h4>
+              <a href="https://github.com/Shinkei/angular-firebase-tutorial">
+                <h4>Angular + Firebase 101</h4>
               </a>
-              <h5>Development</h5>
+              <h5>Angular and firebase tutorial for the angular medellin meetup comunity</h5>
             </div>
           </div>
         </div>
         <div className="col-md-4 photo">
           <div className="portfolio_item">
             <div className="portfolio_img">
-              <img src="img/portfolio/portfolio-5.png" alt="" />
+              <img src="img/portfolio/portfolio-5.png" alt="" width="315" height="280" />
             </div>
             <div className="portfolio_title">
               <a href="#about">
                 <h4>Dreams visualization</h4>
               </a>
-              <h5>Development</h5>
+              <h5>Script to use the pomodoro technique through the terminal</h5>
             </div>
           </div>
         </div>
         <div className="col-md-4 marketing">
           <div className="portfolio_item">
             <div className="portfolio_img">
-              <img src="img/portfolio/portfolio-6.png" alt="" />
+              <img src="img/portfolio/portfolio-6.png" alt="" width="315" height="280" />
             </div>
             <div className="portfolio_title">
               <a href="#about">
                 <h4>Dreams visualization</h4>
               </a>
-              <h5>Development</h5>
+              <h5>Script to use the pomodoro technique through the terminal</h5>
             </div>
           </div>
         </div>
