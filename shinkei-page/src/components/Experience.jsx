60f58cee43397b2696ddd55052221834c363c60f
shinkei <ing.jorgeivanramirezh@gmail.com>
2018-06-28 15:55:39 -0500

    Remove extra complicated css that makes the experience section break
    remove some css clsees that makes the esperience and education section
    to break

--- shinkei-page/src/components/Experience.jsx
+++ shinkei-page/src/components/Experience.jsx
@@ -12,7 +12,14 @@ const Experience = () => (
           <h4>Full Stack Senior Developer</h4>
         </div>
         <h5>Endava</h5>
-        <p>Lorem ipsum dolor sit amets, consectetur adipiscing elit, sed do eiusmod
+        <p><strong>Glowforge Project:</strong> Create, design and develop components made in react,
+           ES6 and redux that consume and interact with an API made in node and CSS3
+          express, also creating unit and integration tests bases on TDD Node.JS
+          <br />
+          <strong>NaviHealth Project:</strong> Maintenance of Angular and Java application in a
+          microservices architecture, with an Oracle database and MongoDB. We
+          used a very organized process driven by CI and scrum to deliver valuable Angular
+          products every sprint.
         </p>
       </div>
       <div className="education_item wow fadeInUp animated" >
@@ -21,7 +28,9 @@ const Experience = () => (
           <h4>Senior Developer</h4>
         </div>
         <h5>Velocity Partners</h5>
-        <p>Lorem ipsum dolor sit amets, consectetur adipiscing elit, sed do eiusmod tempor
+        <p>Perform of a challenging upgrade of a legacy application to new
+           technologies like Java 8, apache 1.8 and Ant 1.9, giving support to SyBase
+           database.
         </p>
       </div>
       <div className="education_item wow fadeInUp animated" >
@@ -30,7 +39,15 @@ const Experience = () => (
           <h4>Java / Python Developer</h4>
         </div>
         <h5>Globant</h5>
-        <p>Lorem ipsum dolor sit amets, consectetur adipiscing elit, sed do eiusmod
+        <p><strong>National Geographic Project:</strong> Maintenance of the the web page for US and
+           UK of the National Geographic Society, builded in Python with the
+           framework django, and JQuery using Postgres and CouchDB as databases,
+           also guided the team dividing the task to develop a new feature.
+          <br />
+          <strong>IOM Project:</strong> Creation and implementation OIM’s Integration System for
+          justice and Peace (SIJYP), build in Java and the framework JBoss SEAM
+          running on the web server JBossAS, contacting the Colombian
+          government to manage a system to help war victims.
         </p>
       </div>
       <div className="education_item wow fadeInUp animated" >
@@ -39,7 +56,10 @@ const Experience = () => (
           <h4>technical Specialist</h4>
         </div>
         <h5>Cobiscorp</h5>
-        <p>Lorem ipsum dolor sit amets, consectetur adipiscing elit, sed do eiusmod
+        <p>Creation of Web Services with the technologies Jax-WS, Jax-RPC, Jax-B, English - Advance
+          WSDLs and XMLs; all this coded in Java using SQL-Server, under
+          WebSphere application server, to communicate remote towns with
+          transactional banking through telephone infrastructure.
         </p>
       </div>
       <div className="education_item wow fadeInUp animated" >
@@ -48,7 +68,10 @@ const Experience = () => (
           <h4>Expert Engineer</h4>
         </div>
         <h5>ITAC</h5>
-        <p>Lorem ipsum dolor sit amets, consectetur adipiscing elit, sed do eiusmod tempor
+        <p>Development and maintenance of business banking applications, in Java
+           EE5,Oracle db, web services and Glassfish application server. Also handle
+           the client’s requirement under pressure to solve production bugs for
+           transactions of high amounts of money.
         </p>
       </div>
       <div className="education_item wow fadeInUp animated" >
@@ -57,7 +80,10 @@ const Experience = () => (
           <h4>Java Developer</h4>
         </div>
         <h5>Datatools</h5>
-        <p>Lorem ipsum dolor sit amets, consectetur adipiscing elit, sed do eiusmod
+        <p>Collaborate in the Design and Development from scratch of an enterprise
+           application using Java EE5, JBoss SEAM, Richfaces and Postgres, and
+           JBOSS AS. This application handles the traffic procedures of different
+           cities in Colombia and Peru.
         </p>
       </div>
     </div>
